1cbe30405d385d0dc8392a3211d8edcd47a2a9f2
Dinakar Nithin <dinakarbnithin@yahoo.com>
2024-04-26 12:25:32 -0500

    Updated week 1 tasks

--- Week1/LittleLemonDB.sql
+++ Week1/LittleLemonDB.sql
@@ -5,6 +5,9 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
 -- Schema LittleLemonDB
 -- -----------------------------------------------------
 
@@ -12,7 +15,28 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- Schema LittleLemonDB
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `LittleLemonDB` DEFAULT CHARACTER SET utf8 ;
+-- -----------------------------------------------------
+-- Schema littlelemondb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema littlelemondb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `littlelemondb` ;
 USE `LittleLemonDB` ;
+
+-- -----------------------------------------------------
+-- Table `LittleLemonDB`.`Customer`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Customer` (
+  `CustomerID` INT NOT NULL,
+  `FullName` VARCHAR(255) NOT NULL,
+  `Contact` VARCHAR(45) NOT NULL,
+  `Email` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`CustomerID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
 
 -- -----------------------------------------------------
 -- Table `LittleLemonDB`.`Bookings`
@@ -21,8 +45,16 @@ CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Bookings` (
   `BookingID` INT NOT NULL AUTO_INCREMENT,
   `Booking_date` DATETIME NOT NULL,
   `TableNumber` INT NOT NULL,
-  PRIMARY KEY (`BookingID`))
-ENGINE = InnoDB;
+  `Customer_ID` INT NOT NULL,
+  PRIMARY KEY (`BookingID`),
+  INDEX `Customer_ID_idx` (`Customer_ID` ASC) VISIBLE,
+  CONSTRAINT `Customer_ID`
+    FOREIGN KEY (`Customer_ID`)
+    REFERENCES `LittleLemonDB`.`Customer` (`CustomerID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
@@ -35,7 +67,8 @@ CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Menu` (
   `Cuisine` VARCHAR(45) NOT NULL,
   `Type` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`MenuID`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
@@ -46,19 +79,8 @@ CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`OrderDelivery` (
   `OrderDel_Date` DATETIME NOT NULL,
   `Order_Status` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`OrderDelivery_ID`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `LittleLemonDB`.`Customer`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Customer` (
-  `CustomerID` INT NOT NULL,
-  `FullName` VARCHAR(255) NOT NULL,
-  `Contact` VARCHAR(45) NOT NULL,
-  `Email` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`CustomerID`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
@@ -70,9 +92,10 @@ CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Staff` (
   `Contact` VARCHAR(45) NOT NULL,
   `Role` VARCHAR(45) NOT NULL,
   `Salary` VARCHAR(45) NOT NULL,
-  `Email` VARCHAR(45) NULL,
+  `Email` VARCHAR(45) NULL DEFAULT NULL,
   PRIMARY KEY (`Staff_ID`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
@@ -82,9 +105,8 @@ CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Orders` (
   `OrderID` INT NOT NULL,
   `OrderDate` DATETIME NOT NULL,
   `Quantity` INT NOT NULL,
-  `TotalCost` DECIMAL(2) NOT NULL,
+  `TotalCost` DECIMAL(2,0) NOT NULL,
   `Book_Booking_ID` INT NOT NULL,
-  `Cust_CustomerID` INT NOT NULL,
   `Menu_MenuID` INT NOT NULL,
   `OrderDel_OrderdelID` INT NOT NULL,
   `Staff_StaffID` INT NOT NULL,
@@ -92,16 +114,15 @@ CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Orders` (
   INDEX `MenuID_idx` (`Menu_MenuID` ASC) VISIBLE,
   INDEX `BookingID_idx` (`Book_Booking_ID` ASC) VISIBLE,
   INDEX `OrderDeliveryID_idx` (`OrderDel_OrderdelID` ASC) VISIBLE,
-  INDEX `CustomerID_idx` (`Cust_CustomerID` ASC) VISIBLE,
   INDEX `StaffID_idx` (`Staff_StaffID` ASC) VISIBLE,
-  CONSTRAINT `MenuID`
-    FOREIGN KEY (`Menu_MenuID`)
-    REFERENCES `LittleLemonDB`.`Menu` (`MenuID`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
   CONSTRAINT `BookingID`
     FOREIGN KEY (`Book_Booking_ID`)
     REFERENCES `LittleLemonDB`.`Bookings` (`BookingID`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `MenuID`
+    FOREIGN KEY (`Menu_MenuID`)
+    REFERENCES `LittleLemonDB`.`Menu` (`MenuID`)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `OrderDeliveryID`
@@ -109,18 +130,78 @@ CREATE TABLE IF NOT EXISTS `LittleLemonDB`.`Orders` (
     REFERENCES `LittleLemonDB`.`OrderDelivery` (`OrderDelivery_ID`)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-  CONSTRAINT `CustomerID`
-    FOREIGN KEY (`Cust_CustomerID`)
-    REFERENCES `LittleLemonDB`.`Customer` (`CustomerID`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
   CONSTRAINT `StaffID`
     FOREIGN KEY (`Staff_StaffID`)
     REFERENCES `LittleLemonDB`.`Staff` (`Staff_ID`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
+USE `littlelemondb` ;
+USE `LittleLemonDB` ;
+
+-- -----------------------------------------------------
+-- procedure CancelOrder
+-- -----------------------------------------------------
+
+DELIMITER $$
+USE `LittleLemonDB`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `CancelOrder`(
+    IN orderId INT
+)
+BEGIN
+    DECLARE rowCount INT;
+
+    DELETE FROM Orders WHERE OrderID = orderId;
+
+    SELECT ROW_COUNT() INTO rowCount;
+
+    IF rowCount > 0 THEN
+        SELECT 'Record with primary key ' + CAST(orderId AS CHAR) + ' deleted successfully.' AS Confrimation;
+    ELSE
+        SELECT 'Record with primary key ' + CAST(orderId AS CHAR) + ' does not exist.' AS Message;
+    END IF;
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure GetMaxQuantity
+-- -----------------------------------------------------
+
+DELIMITER $$
+USE `LittleLemonDB`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `GetMaxQuantity`()
+SELECT MAX(orders.Quantity) as 'Max Quantity in Order'
+FROM orders$$
+
+DELIMITER ;
+USE `littlelemondb` ;
+
+-- -----------------------------------------------------
+-- Placeholder table for view `littlelemondb`.`ordersview`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `littlelemondb`.`ordersview` (`OrderID` INT, `Quantity` INT, `TotalCost` INT);
+
+-- -----------------------------------------------------
+-- Placeholder table for view `littlelemondb`.`ordersview2`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `littlelemondb`.`ordersview2` (`CustomerID` INT, `FullName` INT, `OrderID` INT, `TotalCost` INT);
+
+-- -----------------------------------------------------
+-- View `littlelemondb`.`ordersview`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `littlelemondb`.`ordersview`;
+USE `littlelemondb`;
+CREATE  OR REPLACE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `littlelemondb`.`ordersview` AS select `littlelemondb`.`orders`.`OrderID` AS `OrderID`,`littlelemondb`.`orders`.`Quantity` AS `Quantity`,`littlelemondb`.`orders`.`TotalCost` AS `TotalCost` from `littlelemondb`.`orders` where (`littlelemondb`.`orders`.`Quantity` > 2);
+
+-- -----------------------------------------------------
+-- View `littlelemondb`.`ordersview2`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `littlelemondb`.`ordersview2`;
+USE `littlelemondb`;
+CREATE  OR REPLACE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `littlelemondb`.`ordersview2` AS select `littlelemondb`.`customer`.`CustomerID` AS `CustomerID`,`littlelemondb`.`customer`.`FullName` AS `FullName`,`littlelemondb`.`orders`.`OrderID` AS `OrderID`,`littlelemondb`.`orders`.`TotalCost` AS `TotalCost` from (`littlelemondb`.`customer` join `littlelemondb`.`orders`) where (`littlelemondb`.`orders`.`TotalCost` > 150) order by `littlelemondb`.`orders`.`TotalCost` desc;
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
